5d9f960004074848a752e42eb438e46835b82763
RsvsNeeraj0306 <141002718+RsvsNeeraj0306@users.noreply.github.com>
2023-09-14 11:41:39 +0530

    Delete rental-main directory

--- rental-main/src/main/java/com/cycle/rental/business/LoginBody.java
+++
@@ -1,9 +0,0 @@
-package com.cycle.rental.business;
-
-import lombok.Data;
-
-@Data
-public class LoginBody {
-    private String username;
-    private String password; 
-}
--- rental-main/src/main/java/com/cycle/rental/exception/CycleShopBusinessException.java
+++
@@ -1,9 +0,0 @@
-package com.cycle.rental.exception;
-
-
-
-public class CycleShopBusinessException extends RuntimeException{
-    public CycleShopBusinessException(String message) {
-        super(message);
-    }
-}
